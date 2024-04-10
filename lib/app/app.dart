7bf77764074dbe7caac47c6af59aa2fc5a6f1d1f
Dmitry_Golt <dimagolt@gmail.com>
2024-04-10 21:31:20 +0200

    add basic register screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:solvro_mobile_rekru/app/theme.dart';
 import 'package:solvro_mobile_rekru/feature/login/presentation/bloc/login_cubit.dart';
 import 'package:solvro_mobile_rekru/feature/login/presentation/screens/login_screen.dart';
+import 'package:solvro_mobile_rekru/feature/register/presentation/bloc/register_cubit.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => LoginCubit())
+        BlocProvider(create: (_) => LoginCubit()),
+        BlocProvider(create: (_) => RegisterCubit()),
       ],
       child: MaterialApp(
           title: 'Flutter Demo',
