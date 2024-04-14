c884f65abc3f9b2d8c42213cfd3cbc236370a656
Dmitry_Golt <dimagolt@gmail.com>
2024-04-14 18:36:31 +0200

    create mock list screen

---
+++ lib/feature/list/presentation/screens/list_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:solvro_mobile_rekru/shared/domain/entities/shopping_list.dart';
+
+class ListScreen extends StatelessWidget {
+  final ShoppingList list;
+
+  const ListScreen({super.key, required this.list});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: list.color,
+        title: Text(list.name),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
+        ],
+      ),
+      body: Container(
+        color: Colors.white,
+        child: const Center(
+          child: Text('items'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: list.color,
+        onPressed: () {},
+        label: const Row(
+          children: [
+            Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            Text(
+              'Add items',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
