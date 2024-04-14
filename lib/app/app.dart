796fd2a3d66d9411893f0cde0ddd1d50736384bd
Dmitry_Golt <dimagolt@gmail.com>
2024-04-14 14:43:12 +0200

    add mocked main screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:solvro_mobile_rekru/app/theme.dart';
 import 'package:solvro_mobile_rekru/feature/login/presentation/bloc/login_cubit.dart';
 import 'package:solvro_mobile_rekru/feature/login/presentation/screens/login_screen.dart';
+import 'package:solvro_mobile_rekru/feature/main/presentation/bloc/shopping_list_bloc.dart';
 import 'package:solvro_mobile_rekru/feature/register/presentation/bloc/register_cubit.dart';
 import 'package:solvro_mobile_rekru/shared/domain/repositories/auth_repository.dart';
 
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
         providers: [
           BlocProvider(create: (ctx) => LoginCubit(ctx.read<AuthRepository>())),
           BlocProvider(create: (ctx) => RegisterCubit(ctx.read<AuthRepository>())),
+          BlocProvider(create: (ctx) => ShoppingListBloc())
         ],
         child: MaterialApp(
             title: 'Flutter Demo',
--- lib/feature/main/presentation/screens/main_screen.dart
+++ lib/feature/main/presentation/screens/main_screen.dart
@@ -1,15 +1,58 @@
+import 'package:bloc_widgets/bloc_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:solvro_mobile_rekru/shared/domain/repositories/auth_repository.dart';
+import 'package:solvro_mobile_rekru/feature/main/presentation/bloc/shopping_list_bloc.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen
+    extends BlocConsumerWidget<ShoppingListBloc, ShoppingListState> {
   const MainScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget buildWithState(context, bloc, state) {
     return Scaffold(
-      body: Center(
-        child: Text('Hi ${context.read<AuthRepository>().user!.id}'),
+      appBar: AppBar(
+        title: const Text('My shopping lists'),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: ListView.separated(
+          itemCount: 5,
+          itemBuilder: (_, index) => ListTile(
+            leading: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  width: 15,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.red,
+                  ),
+                ),
+                const Text(
+                  '🐞',
+                  style: TextStyle(fontSize: 32),
+                ),
+              ],
+            ),
+            title: Text('List $index'),
+          ),
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(height: 1),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Row(
+          children: [
+            Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            Text(
+              'Create new',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
       ),
     );
   }
